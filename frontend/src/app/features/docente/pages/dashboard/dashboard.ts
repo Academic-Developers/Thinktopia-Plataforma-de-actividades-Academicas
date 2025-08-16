3d1644daa810bcc16f21ec5e97dba5e6389059e2
Cesia Fiorella Cáceres Giménez <cesiaf.gimenez@gmail.com>
2025-08-16 13:19:20 -0300

    chore: agregar estructura HTML base desde el maquetado inicial

--- frontend/src/app/features/docente/pages/dashboard/dashboard.ts
+++ frontend/src/app/features/docente/pages/dashboard/dashboard.ts
@@ -1,7 +1,12 @@
+
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DashboardDocenteHeader } from '../../components/dashboard-docente-header/dashboard-docente-header';
+import { DashboardDocenteFooter } from '../../components/dashboard-docente-footer/dashboard-docente-footer';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
