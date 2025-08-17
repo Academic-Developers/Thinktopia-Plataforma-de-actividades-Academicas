97fc2feeb83aef35bc3c812953e586ff54ab6220
Cesia Fiorella Cáceres Giménez <cesiaf.gimenez@gmail.com>
2025-08-16 22:54:26 -0300

    chore(icons): agregar nuevos íconos a la carpeta public/icons

---
+++ frontend/src/app/features/docente/pages/estadisticas/estadisticas.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadisticas',
+  templateUrl: './estadisticas.html',
+  styleUrls: ['./estadisticas.css']
+})
+export class Estadisticas{}
