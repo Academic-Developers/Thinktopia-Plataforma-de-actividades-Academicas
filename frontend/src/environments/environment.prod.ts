ffa33e10fd736efcf04c7c4e8e300b4c9ead22da
Cesia Fiorella Cáceres Giménez <cesiaf.gimenez@gmail.com>
2025-09-10 00:34:11 -0300

    fix: corregir URLs en environments - quitar slash final

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://reqres.in/api/',
   apiKey: '',
   urlReqres: 'https://reqres.in/api/',
-  urlJsonServer: 'http://localhost:3000/'
+  urlJsonServer: 'http://localhost:3000'
 };
