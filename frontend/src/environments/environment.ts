5ba19040f1cec914782048c2efdafda0c3946922
Cesia Fiorella Cáceres Giménez <cesiaf.gimenez@gmail.com>
2025-09-13 20:42:00 -0300

    chore(env): actualiza configuración de entorno

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://reqres.in/api/',
-  apiKey: '',
   urlReqres: 'https://reqres.in/api/',
-  urlJsonServer: 'http://localhost:3000'
+  urlJsonServer: 'http://localhost:3000/',
+  apiKey: 'reqres-free-v1'
 };
