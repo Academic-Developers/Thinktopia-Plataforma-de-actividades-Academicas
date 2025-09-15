96e1c5ada9d6e957137e41977cdb767b6308698c
Cesia Fiorella Cáceres Giménez <cesiaf.gimenez@gmail.com>
2025-09-14 21:44:04 -0300

    update gitignore

--- frontend/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://reqres.in/api/',
-  apiKey: '',
-  urlReqres: 'https://reqres.in/api/',
-  urlJsonServer: 'http://localhost:3000'
-};
--- frontend/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  urlReqres: 'https://reqres.in/api/',
-  urlJsonServer: 'http://localhost:3000/',
-  apiKey: 'reqres-free-v1'
-};
