ba6ea549ca29246734cf3bd440db586e1bd5cd59
Francisco <torofrancisco13@gmail.com>
2025-09-29 04:18:34 -0300

    feat: API Rest - Login View and Endpoint

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UsuarioRegistroSerializer
+from .models import Usuario 
 
 # Register "Controlador" para usar en el Endpoint que se declara en urls.py
 class RegistroUsuarioAPIView(APIView):
@@ -28,3 +29,43 @@ class RegistroUsuarioAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# Login
+
+class LoginUsuarioAPIView(APIView):
+    """
+    Vista para autenticar usuarios.
+    """
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        # Verificamos que ambos campos estén presentes en la request
+        if not email or not password:
+            return Response(
+                {"error": "Se requiere 'email' y 'password' para iniciar sesión."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Verificamos que exista el usuario en la base de datos y este todo bien para luego responder devolviendo el id, email, role con un codigo HTTP 200 o en caso de algun problema los 400.
+        try:
+            # Buscamos al usuario por email
+            usuario = Usuario.objects.get(email=email)
+
+            # Verificamos la contraseña
+            if usuario.check_password(password):
+                return Response({
+                    "id": usuario.id,
+                    "email": usuario.email,
+                    "role": usuario.role
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    {"error": "Credenciales inválidas. Verifique su email y contraseña."},
+                    status=status.HTTP_401_UNAUTHORIZED
+                )
+        except Usuario.DoesNotExist:
+            return Response(
+                {"error": "Usuario no encontrado."},
+                status=status.HTTP_404_NOT_FOUND
+            )
